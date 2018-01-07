4e3e93eaab7c5ba8d97707377ab080539b6620e9
Linh Do <luxiliph@gmail.com>
2018-01-07 04:41:31 +0100

    coordinate system size relative to window

--- js/co-system.js
+++ js/co-system.js
@@ -7,11 +7,20 @@ function makeCoSystem() {
     var selector;
     var svg;
 
-    var width;
-    var height;
+    //variables for a flexible coordinate system
+    var minX;
+    var minY;
+    var maxX;
+    var maxY;
+    //variable to check whether there should be a negative and postive x-axis
+    var bPositiveAndNegativeXAxis;
+    //variable to check whether there should be a negative and postive y-axis
+    var bPositiveAndNegativeYAxis;
+
     var points;
+    var sample;
 
-
+    var radius = 5;
 
 
     function init(sl) {
@@ -24,85 +33,95 @@ function makeCoSystem() {
         var arr = txtString.split(" ");
         points = [];
 
-        width = 0;
-        height = 0;
+        maxX = -2147483648;
+        maxY = -2147483648;
+        minX = 2147483647;
+        minY = 2147483647;
         for (var i = 0; i<arr.length; i+=2){
-            points.push([arr[i], arr[i+1]]);
-            width = Math.max(width, arr[i]);
-            height = Math.max(height, arr[i+1]);
+            var point = {x: arr[i], y: arr[i+1], onBoundary: false}
+            points.push(point);
+            maxX = Math.max(maxX, arr[i]);
+            maxY = Math.max(maxY, arr[i+1]);
+            minX = Math.min(minX, arr[i]);
+            minY = Math.min(minY, arr[i+1]);
         }
-        $('#status').text(width);
-        createCanvas();
+
+        createCanvas(updateGraph);
     }
 
-    function createCanvas() {
+    function createCanvas(cb) {
         var canvasElement = $("<div/>");
+        var width = 0;
+        var height = 0;
+
+        bPositiveAndNegativeXAxis = (maxX*minX < 0);
+        bPositiveAndNegativeYAxis = (maxY*minY < 0);
+
+        if(bPositiveAndNegativeXAxis)   width = maxX + Math.abs(minX);
+        else                            width = Math.abs(maxX + minX);
+
+
+        if(bPositiveAndNegativeYAxis)   height = maxY + Math.abs(minY);
+        else                            height = Math.abs(maxY + minY);
+
+
+        var relWidth = window.innerWidth ? window.innerWidth : $(window).width();
+        var relHeight = window.innerHeight ? window.innerHeight : $(window).height();
+        $('#status').text(relWidth);
+
         svg = d3.select(selector).append("svg")
             .attr("id", "svg")
-            .attr("width", width+40)
-            .attr("height", height+40)
+            .attr("width", relWidth/4*3)
+            .attr("height", relHeight/4*3)
         ;
-
-        //Create the Scale we will use for the Axis
-        var axisScale = d3.scale.linear().domain([0, width]).range([0, width]);
-        var yAxisScale = d3.scale.linear().domain([0, height]).range([0, height]);
-
-        // Create the Axis
-        var xAxis = d3.svg.axis()
-            .scale(axisScale)
-            .tickValues([50,100,150,200])
-            .tickSize(10, 0)
-        ;
-
-        var yAxis = d3.svg.axis()
-            .orient("right")
-            .scale(yAxisScale)
-            .tickValues([50,100,150,200])
-            .tickSize(10, 0)
-        ;
-
-        //Create an SVG group Element for the Axis elements and call the xAxis function
-        svg.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")")
-            .call(xAxis)
-            .selectAll(".tick")
-            .style("stroke", "black")
-            .selectAll(".tick text")
-            .style("text-anchor", "start")
-            .attr("x", -13)
-            .attr("y", 15)
-            .attr("font-size", "12")
-
-        ;
-
-        //Create an SVG group Element for the Axis elements and call the xyxis function
-        svg.append("g")
-            .attr("class", "y axis")
-            .attr("transform", "translate(" + height + ",0)")
-            .call(yAxis)
-            .selectAll(".tick")
-            .style("stroke", "black")
-            .selectAll(".tick text")
-            .style("text-anchor", "start")
-            .attr("x", 13)
-            .attr("y", -5)
-            .attr("font-size", "12")
-        ;
-
-
+        
         var borderPath = svg.append("rect")
             .attr("x", 0)
             .attr("y", 0)
-            .attr("width", width)
-            .attr("height", height)
+            .attr("width", relWidth/4*3)
+            .attr("height", relHeight/4*3)
             .style("stroke", "#373737")
             .style("fill", "none")
             .style("stroke-width", 1)
         ;
 
+        sample = svg.selectAll(".sample-node");
+        cb();
+
         return canvasElement;
     }
+    
+    function updateGraph() {
+        sample = sample.data(points);
+        sample.enter().append("circle")
+            .attr("class", "sample-node")
+            .attr("r", radius)
+            .attr("transform", function(d) {
+                var x;
+                var y;
+                if(bPositiveAndNegativeXAxis)   x = parseInt(d.x) + Math.abs(minX);
+                else                            x = d.x;
+                if(bPositiveAndNegativeYAxis)   y = parseInt(d.y) + Math.abs(minY);
+                else                            y = d.y;
+
+                return "translate(" + x + "," + y + ")"
+            })
+            .attr("id", function (d,i) {
+                return "c_"+i;
+            })
+            .style("fill", function(d) {
+                if (d.onBoundary === false)     return "#000000";
+                else                            return "#ff0000";
+            })
+            .style("cursor", "pointer");
+        sample.exit().remove();
+    }
+
+
+    var scaleX = d3.scale.linear()
+        .domain([10, 130])
+        .range([0, 960]);
+
 
     return {
         init: function (selector) {
